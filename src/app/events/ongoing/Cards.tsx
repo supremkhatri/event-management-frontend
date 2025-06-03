a56cd4e0834331700e46881cf21104bfd934e5e2
Suprem Khatri <supremkhatri@Suprems-MacBook-Pro.local>
2025-06-03 17:39:00 +0545

    Improved Images

--- src/app/events/ongoing/Cards.tsx
+++ src/app/events/ongoing/Cards.tsx
@@ -12,11 +12,11 @@ export default function OngoingCard({
   image: string;
 }) {
   return (
-    <div className="bg-white shadow-md hover:shadow-xl transition-shadow transform hover:-translate-y-1 rounded-lg overflow-hidden">
+    <div className="bg-blue-950 max-w-sm shadow-md hover:shadow-xl transition-shadow transform hover:-translate-y-1 rounded-lg overflow-hidden">
       <Image src={image} alt={title} width={250} height={250} className="w-full  object-cover" />
       <div className="p-6">
-        <h3 className="text-xl font-semibold text-blue-900">{title}</h3>
-        <p className="text-gray-700 mt-2 text-sm">{description}</p>
+        <h3 className="text-xl font-semibold text-white">{title}</h3>
+        <p className="text-white text-sm">{description}</p>
         <div className="mt-4 flex justify-between items-center">
         <Link href={`/events/ongoing/${title.replace(/\s+/g, '-')}`}>
             <button className="px-4 py-2 bg-blue-600 text-white text-sm font-semibold rounded-lg shadow hover:bg-blue-700 transition">
@@ -24,7 +24,7 @@ export default function OngoingCard({
             </button>
           </Link>
           <Link href={`/register/${title.replace(/\s+/g, '-')}`}>
-            <button className="px-4 py-2 border border-blue-600 text-blue-600 text-sm font-semibold rounded-lg hover:bg-blue-100 transition">
+            <button className="px-4 py-2 border border-white text-white text-sm font-semibold rounded-lg hover:bg-blue-100 hover:text-blue-700 transition">
               Register Now
             </button>
           </Link>
--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -7,10 +7,10 @@ export default function Events() {
   const pastEvents = EVENTS.filter((event) => event.status === 'past');
 
   return (
-    <div className="bg-black-200 min-h-screen py-8">
+    <div className="bg-black-200 flex flex-col items-center text-center min-h-screen py-8">
 
       {/* Ongoing Events */}
-      <div className="mt-8">
+      <div className="mt-10">
         <h2 className="text-4xl font-extrabold text-center text-blue-400 mb-8">Ongoing Events</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 px-4 lg:px-12">
           {ongoingEvents.map((event, index) => (
